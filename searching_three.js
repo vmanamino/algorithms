5a2febc81ce6b103c9a799f3bbe76e82b06966cd
vmanamino <vmanamino@gmail.com>
2016-07-06 03:35:18 +0000

    done

--- searching_three.js
+++ searching_three.js
@@ -8,16 +8,21 @@
 
 var item = 212;
 
-var item2 = 013;
+
 
 var checkFast = function(item){
+  console.log('item')
+  console.log(item) 
   ddclass = 0;
   if (item == 100){
     console.log('found')
-  }
+  }  
   else {
     var specific = 0;
+    console.log('modulo')
+    console.log(item%10)
     var itemSpecific = item % 10;
+    console.log(itemSpecific+'item spec')
     ddclass = item - (item % 100)    
     var dddiv = (item % 100) - ((item % 100) % 10) 
     var main = 0;
@@ -29,6 +34,8 @@ var checkFast = function(item){
         }
         main += 100
       }
+    }
+    if (item > 100 || item < 100){
       while (div < 100){
         if (dddiv == div){
           console.log(div)
@@ -43,9 +50,10 @@ var checkFast = function(item){
         }
         specific++;
       }
-    }
+    }     
   }
+  
   return (ddclass/100)+''+(dddiv/10)+''+itemSpecific;
 }
 
-console.log(checkFast(item));
+console.log(checkFast(item))
